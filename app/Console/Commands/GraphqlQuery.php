2c503607ab6131313ab8f46d7474b370e60505e6
Markus Fischer <markus@fischer.name>
2019-07-06 23:48:59 +0200

    Add graphql:query command

---
+++ app/Console/Commands/GraphqlQuery.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use GraphQL\GraphQL;
+use Illuminate\Console\Command;
+
+class GraphqlQuery extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'graphql:query';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $graphql = <<<'GRAPHQL'
+{
+  test {
+    id
+  }
+}
+GRAPHQL;
+
+        $result = \Rebing\GraphQL\Support\Facades\GraphQL::query($graphql);
+
+        echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES), "\n"; exit;
+    }
+}
